d39d88e26d65a27b64f6c51d2a7e250433ec5398
razisamuely <razsh1212@gmail.com>
2022-12-26 16:49:30 +0200

    test file

--- test.py
+++ test.py
@@ -1,30 +1,39 @@
+"""
+Playing around with MPE PettingZoo Environments: https://www.pettingzoo.ml/mpe
+"""
+
 import numpy as np
 from pettingzoo.utils import random_demo
-# from pettingzoo.mpe import simple_adversary_v2
+from pettingzoo.mpe import simple_adversary_v2
 from pettingzoo.mpe import simple_tag_v2
 # from pettingzoo.magent import adversarial_pursuit_v3
 
-def tag():
-    env = simple_tag_v2.env(num_good=2, num_adversaries=1, num_obstacles=1, render_mode='human')
-    env.reset()
-    for i in range (1000):
-        x = 0
-        while x < 1:
-            for agent in env.agent_iter():
-                observation, reward, done, info, _ = env.last()
-                if done:
-                    action = None
-                    env.reset()
-                    print("DONE>>>>")
-                    break
-                else:
-                    action = env.action_space(agent).sample()
-                    #print("ACTION = ", action)
-                env.step(action)
-                #env.step(0)
-                print(agent, " >>> ",  len(observation), observation)
+def tag(render=True, episodes=10):
+    env = simple_tag_v2.env(num_good=1, num_adversaries=1, num_obstacles=1, render_mode='human')
+    """Runs an env object with random actions."""
+    total_reward = 0
+    completed_episodes = 0
+
+    while completed_episodes < episodes:
+        env.reset()
+        for agent in env.agent_iter():
+            if render:
                 env.render()
-            x += 1
+
+            obs, reward, termination, truncation, _ = env.last()
+            total_reward += reward
+            if termination or truncation:
+                action = None
+            elif isinstance(obs, dict) and "action_mask" in obs:
+                action = random.choice(np.flatnonzero(obs["action_mask"]))
+            else:
+                action = env.action_space(agent).sample()
+            env.step(action)
+
+        completed_episodes += 1
+
+    if render:
+        env.close()
 
 def demo_adversary():
     env = simple_adversary_v2.env()
@@ -36,10 +45,10 @@ def demo_adv_pursuit():
 
 def demo_tag():
     env = simple_tag_v2.env(num_good=4, num_adversaries=4, num_obstacles=3, render_mode='human')
-    random_demo(env, render=True, episodes=100)
+    random_demo(env, render=True, episodes=10)
 
 if __name__ == '__main__':
     tag()
-  #  demo_adversary()
+    #demo_adversary()
     #demo_adv_pursuit()
-    #demo_tag()
+    # demo_tag()
